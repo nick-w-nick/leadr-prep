7a968c320b3ce63343259f40eec434795188dbee
nick-w-nick <43578531+nick-w-nick@users.noreply.github.com>
2022-02-17 13:22:45 -0500

    added helmet + updated static path + moved chalk import

--- app.js
+++ app.js
@@ -3,16 +3,18 @@ import 'dotenv/config';
 import express from 'express';
 const app = express();
 
-import chalk from 'chalk';
-
 app.use(express.json({ limit: '30mb', extended: true }));
 app.use(express.urlencoded({ extended: true }));
 
+import helmet from 'helmet';
+app.use(helmet());
+
 import { resolve } from 'path';
-app.use(express.static(`${resolve()}/static`, { dotfiles: 'allow' }));
+app.use('/static', express.static(`${resolve()}/static`, { dotfiles: 'allow' }));
 
 app.enable('trust proxy');
 
+import chalk from 'chalk';
 app.listen(process.env.PORT || 3000, async (err) => {
     if (err) {
         return console.log(chalk.red(`Error starting the server ❌\n${err}`));
